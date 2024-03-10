9c49dd5a56a5837d351ac9af7a0e312b9410b867
Lukasz Lenart <lukaszlenart@apache.org>
2024-03-10 12:05:40 +0100

    Uses Gradle RAT plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.8.21"
     id("org.jetbrains.intellij") version "1.13.3"
+    id("org.nosphere.apache.rat") version "0.8.1"
 }
 
 group = "com.intellij"
@@ -54,5 +55,25 @@ tasks {
 
     publishPlugin {
         token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+
+    rat {
+        // Input directory, defaults to '.'
+        inputDir.set(file("src/main/java"))
+        inputDir.set(file("src/main/resources"))
+        inputDir.set(file("src/test/java"))
+        inputDir.set(file("src/test/testData"))
+
+        // List of Gradle exclude directives, defaults to ['**/.gradle/**']
+        excludes.add("**/build/**")
+
+        // Rat excludes file, one directive per line
+        // excludeFile.set(layout.projectDirectory.file(".rat-excludes.txt"))
+
+        // Fail the build on rat errors, defaults to true
+        failOnError.set(false)
+
+        // Prints the list of files with unapproved licences to the console, defaults to false
+        verbose.set(true)
     }
 }
