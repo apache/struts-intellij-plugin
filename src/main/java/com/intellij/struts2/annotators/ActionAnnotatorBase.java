5d3fd2e3f6ec344b04ea03ba706c74f80bf092bc
Lukasz Lenart <lukaszlenart@apache.org>
2024-12-02 20:06:56 +0100

    Lowers top supported version

--- src/main/java/com/intellij/struts2/annotators/ActionAnnotatorBase.java
+++ src/main/java/com/intellij/struts2/annotators/ActionAnnotatorBase.java
@@ -16,7 +16,6 @@ package com.intellij.struts2.annotators;
 
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo;
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider;
-import com.intellij.codeInsight.navigation.DomNavigationUtil;
 import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder;
 import com.intellij.icons.AllIcons;
 import com.intellij.navigation.GotoRelatedItem;
@@ -150,8 +149,8 @@ public abstract class ActionAnnotatorBase extends RelatedItemLineMarkerProvider 
         StrutsBundle.message("annotators.action.goto.tooltip");
     final NavigationGutterIconBuilder<DomElement> gutterIconBuilder =
         NavigationGutterIconBuilder.create(Struts2Icons.Action,
-                        DomNavigationUtil.DEFAULT_DOM_CONVERTOR,
-                        DomNavigationUtil.DOM_GOTO_RELATED_ITEM_PROVIDER)
+                        NavigationGutterIconBuilder.DEFAULT_DOM_CONVERTOR,
+                        NavigationGutterIconBuilder.DOM_GOTO_RELATED_ITEM_PROVIDER)
                                    .setAlignment(GutterIconRenderer.Alignment.LEFT)
                                    .setPopupTitle(StrutsBundle.message("annotators.action.goto.declaration"))
                                    .setTargets(actions)
